db37c1c5a9b8d68ca2b142b1c4e87dbd3e0178a5
Steve Mactaggart <steve@whitesquaresoft.com>
2024-06-04 20:00:42 +1000

    Added support for ECS Runtime protection messages.

--- test/parsers/guardduty-runtime.test.js
+++ test/parsers/guardduty-runtime.test.js
@@ -4,7 +4,7 @@
 // Update these examples below if they happen to match your custom parser format.
 
 const findingsMock = require("./_parser_mock").named("guardduty-findings");
-const runtimeMock = require("./_parser_mock").named("guardduty-kuberuntime");
+const runtimeMock = require("./_parser_mock").named("guardduty-runtime");
 
 const simpleSnsPacket = {
 	Records: [{
@@ -101,5 +101,177 @@ runtimeMock.matchesEventWithDetail(healthyRuntimeProtection, {
 	"author_name": "Amazon GuardDuty",
 	"color": "good",
 	"fallback": "GuardDuty Runtime Protection Healthy ",
-	"title": "GuardDuty Runtime Protection Healthy"
+	"title": "GuardDuty Runtime Protection Healthy",
+	"fields": [
+		{
+			"short": true,
+			"title": "Account",
+			"value": "EXAMPLE",
+		},
+		{
+			"short": true,
+			"title": "Region",
+			"value": "ap-southeast-2",
+		},
+		{
+			"short": true,
+			"title": "Resource Type",
+			"value": "EC2",
+		},
+		{
+			"short": true,
+			"title": "Instance",
+			"value": "i-0e5f6345341fd7144",
+		},
+		{
+			"short": true,
+			"title": "Instance Type",
+			"value": "t3.medium",
+		},
+		{
+			"short": false,
+			"title": "ClusterArn",
+			"value": "arn:aws:eks:ap-southeast-2:EXAMPLE:cluster/eks-demo",
+		},
+		{
+			"short": true,
+			"title": "Agent Version",
+			"value": "v1.6.1",
+		},
+		{
+			"short": true,
+			"title": "Management Type",
+			"value": "MANUAL",
+		},
+	]
+});
+
+const healthyECSGuardDuty = {
+	"Records": [
+		{
+			"EventSource": "aws:sns",
+			"EventVersion": "1.0",
+			"EventSubscriptionArn": "arn:aws:sns:ap-southeast-2:EXAMPLE:aws-to-slack:6635ff10-c98f-455b-90f7-9b8386082633",
+			"Sns": {
+				"Type": "Notification",
+				"MessageId": "b07dddbc-90ef-59d9-8a06-83643fa2dbc1",
+				"TopicArn": "arn:aws:sns:ap-southeast-2:EXAMPLE:aws-to-slack",
+				"Subject": null,
+				"Message": "{\"version\":\"0\",\"id\":\"a0b44948-7c77-790e-b927-481427f6a97b\",\"detail-type\":\"GuardDuty Runtime Protection Healthy\",\"source\":\"aws.guardduty\",\"account\":\"EXAMPLE\",\"time\":\"2024-06-04T05:58:35Z\",\"region\":\"ap-southeast-2\",\"resources\":[],\"detail\":{\"accountId\":\"EXAMPLE\",\"resourceDetails\":{\"resourceType\":\"ECS\",\"ecsClusterDetails\":{\"clusterName\":\"buildkite-deploy-dev-Cluster-1O47RNJPQMRI6-EcsCluster-RPPZMvAnkFJP\",\"fargateDetails\":{\"issues\":[],\"managementType\":\"AUTO_MANAGED\"},\"containerInstanceDetails\":{\"coveredContainerInstances\":0,\"compatibleContainerInstances\":0}}},\"previousStatus\":\"Unhealthy\",\"currentStatus\":\"Healthy\",\"issue\":\"\",\"lastUpdatedAt\":1717480351457}}",
+				"Timestamp": "2024-06-04T05:58:37.795Z",
+				"SignatureVersion": "1",
+				"Signature": "EXAMPLE",
+				"SigningCertUrl": "EXAMPLE",
+				"UnsubscribeUrl": "EXAMPLE",
+				"MessageAttributes": {}
+			}
+		}
+	]
+}
+
+runtimeMock.matchesEvent(healthyECSGuardDuty);
+
+runtimeMock.matchesEventWithDetail(healthyECSGuardDuty, {
+	"author_name": "Amazon GuardDuty",
+	"color": "good",
+	"fallback": "GuardDuty Runtime Protection Healthy ",
+	"title": "GuardDuty Runtime Protection Healthy",
+	"fields": [
+		{
+			"short": true,
+			"title": "Account",
+			"value": "EXAMPLE",
+		},
+		{
+			"short": true,
+			"title": "Region",
+			"value": "ap-southeast-2",
+		},
+		{
+			"short": true,
+			"title": "Resource Type",
+			"value": "ECS",
+		},
+		{
+			"short": true,
+			"title": "Cluster Name",
+			"value": "buildkite-deploy-dev-Cluster-1O47RNJPQMRI6-EcsCluster-RPPZMvAnkFJP",
+		},
+		{
+			"short": false,
+			"title": "Issue",
+			"value": [],
+		},
+		{
+			"short": true,
+			"title": "Management Type",
+			"value": "AUTO_MANAGED",
+		},
+	]
+});
+
+
+const unhealthyECSEvent = {
+	"Records": [
+		{
+			"EventSource": "aws:sns",
+			"EventVersion": "1.0",
+			"EventSubscriptionArn": "arn:aws:sns:ap-southeast-2:EXAMPLE:aws-to-slack:6635ff10-c98f-455b-90f7-9b8386082633",
+			"Sns": {
+				"Type": "Notification",
+				"MessageId": "daa6ae66-99d9-5e51-853f-f0b1e003cbfa",
+				"TopicArn": "arn:aws:sns:ap-southeast-2:EXAMPLE:aws-to-slack",
+				"Subject": null,
+				"Message": "{\"version\":\"0\",\"id\":\"7e706286-0e00-7515-f2be-0793fd3092d9\",\"detail-type\":\"GuardDuty Runtime Protection Unhealthy\",\"source\":\"aws.guardduty\",\"account\":\"EXAMPLE\",\"time\":\"2024-06-03T07:58:16Z\",\"region\":\"ap-southeast-2\",\"resources\":[],\"detail\":{\"accountId\":\"EXAMPLE\",\"resourceDetails\":{\"resourceType\":\"ECS\",\"ecsClusterDetails\":{\"clusterName\":\"buildkite-deploy-dev-Cluster-1O47RNJPQMRI6-EcsCluster-RPPZMvAnkFJP\",\"fargateDetails\":{\"issues\":[\"Others : Unidentified issue(s), for task(s) in TaskDefinition 'buildkite:17' . Refer documentation\"],\"managementType\":\"AUTO_MANAGED\"},\"containerInstanceDetails\":{\"coveredContainerInstances\":0,\"compatibleContainerInstances\":0}}},\"previousStatus\":\"Healthy\",\"currentStatus\":\"Unhealthy\",\"issue\":\"\",\"lastUpdatedAt\":1717401173897}}",
+				"Timestamp": "2024-06-03T07:58:18.667Z",
+				"SignatureVersion": "1",
+				"Signature": "EXAMPLE",
+				"SigningCertUrl": "EXAMPLE",
+				"UnsubscribeUrl": "EXAMPLE",
+				"MessageAttributes": {}
+			}
+		}
+	]
+}
+
+
+runtimeMock.matchesEvent(unhealthyECSEvent);
+
+runtimeMock.matchesEventWithDetail(unhealthyECSEvent, {
+	"author_name": "Amazon GuardDuty",
+	"color": "danger",
+	"fallback": "GuardDuty Runtime Protection Unhealthy ",
+	"title": "GuardDuty Runtime Protection Unhealthy",
+	"fields": [
+		{
+			"short": true,
+			"title": "Account",
+			"value": "EXAMPLE",
+		},
+		{
+			"short": true,
+			"title": "Region",
+			"value": "ap-southeast-2",
+		},
+		{
+			"short": true,
+			"title": "Resource Type",
+			"value": "ECS",
+		},
+		{
+			"short": true,
+			"title": "Cluster Name",
+			"value": "buildkite-deploy-dev-Cluster-1O47RNJPQMRI6-EcsCluster-RPPZMvAnkFJP",
+		},
+		{
+			"short": false,
+			"title": "Issue",
+			"value": [ "Others : Unidentified issue(s), for task(s) in TaskDefinition 'buildkite:17' . Refer documentation",],
+		},
+		{
+			"short": true,
+			"title": "Management Type",
+			"value": "AUTO_MANAGED",
+		},
+	]
 });
