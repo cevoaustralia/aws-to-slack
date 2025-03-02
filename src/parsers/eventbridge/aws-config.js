ded02318635483c8e6f008ccd29e1712d4535068
abdullahshafiq1 <abdullah.shafiq@insigniafinancial.com.au>
2025-03-03 10:53:58 +1100

    updated parser matcher and fields

--- src/parsers/eventbridge/aws-config.js
+++ src/parsers/eventbridge/aws-config.js
@@ -2,21 +2,24 @@
 // AWS Config event parser
 //
 exports.matches = event =>
-    _.has(event.message, "config");
+    _.has(event.message, "aws-config");
 
 exports.parse = event => {
     const accountId = event.get("accountId");
     const region = event.get("awsRegion");
-    const configItem = event.get("Message");
-    const resourceType = configItem.resourceType;
-    const resourceId = configItem.resourceId;
-    const resourceName = configItem.resourceName || resourceId;
-    const configurationItemStatus = configItem.configurationItemStatus;
+//    const configItem = event.get("Message");
+  //  const resourceType = configItem.resourceType;
+    const resourceId = event.get("resourceId");
+    const detailType = event.get("detail-type");
+    const configRuleName = event.get("configRuleName");
+    //    const resourceId = configItem.resourceId;
+//    const resourceName = configItem.resourceName || resourceId;
+//    const configurationItemStatus = configItem.configurationItemStatus;
     
     const signInLink = `https://${accountId}.signin.aws.amazon.com/console/config?region=${region}`;
     const consoleLink = event.consoleUrl(`/config/home?region=${region}`);
 
-    const text = `AWS Config detected a ${configurationItemStatus} change for resource ${resourceType}`;
+    const text = `AWS Config detected a ${detailType} change for resource ${resourceId}`;
 
     return event.attachmentWithDefaults({
         author_name: `AWS Config (${region} - ${accountId})`,
@@ -24,7 +27,7 @@ exports.parse = event => {
         title: `${resourceName} - ${configurationItemStatus}`,
         title_link: consoleLink,
         text,
-        fallback: text,
+        //fallback: text,
         color: event.COLORS.warning,
         fields: [{
             title: "Resource Type",
